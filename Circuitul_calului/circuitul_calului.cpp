96aca4c8ac169ab7f75846aee41888cf099cb8bf
Denisa Jurca <denisa.jurca@student.upt.ro>
2022-12-19 18:30:55 +0200

    3

--- Circuitul_calului/circuitul_calului.cpp
+++ Circuitul_calului/circuitul_calului.cpp
@@ -2,15 +2,18 @@
 
 using namespace std;
 
-const int n = 5;
-int a[n][n];
-int i_init,j_init;//pozitii initiale i si j
+int n ;
+int a[100][100];
+int i_init,j_init;
 const int directii = 8;
-int v_i[directii] = { -1,-2,-2,-1,1,2,2,1 };
-int v_j[directii] = { -2,-1,1,2,2,1,-1,-2 };
+int v_i[directii] = { -2, -1, 1, 2,  2,  1, -1, -2 };
+int v_j[directii] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+int i, j;
 
 void Citire()
 {
+	cout << "Dati marimea tablei de sah:";
+	cin >> n;
 	cout << "Introduceti pozitia initiala pentru i:";
 	cin >> i_init;
 	cout << "Introduceti pozitia initiala pentru j:";
@@ -20,62 +23,58 @@ void Citire()
 
 void Afisare()
 {
-	int i, j;
-	for (i = 1; i < n; i++)
+	for (i =0; i < n; i++)
 	{
 		cout << endl;
-		for (j = 1; j < n; j++)
+		for (j = 0; j < n; j++)
 			cout << a[i][j] << " ";
 	}
-	cout << endl;
+
 }
 
 int Verificare_poz(int i, int j)
 {
-	if (i >=n || i <= 0)
+	if (i >n-1 || i < 0)
 		return 0;
-	if (j >=n || j <= 0)
+	if (j >n-1 || j < 0)
+		return 0;
+	if (a[i][j] !=0)
 		return 0;
 
-	if (a[i][j] == 0)
-		return 1;
-
-	if (a[i][j] != 0)
-		return 0;
+	return 1;
 }
 
-int Mutare_cal(int i, int j, int pas)
+void Mutare_cal(int i, int j, int pas)
 {
 	int k;
 	int i_vecin;
 	int j_vecin;
 
-	if (pas == n*n+1)
-		return 0;
-	else
+	if (pas==(n*n)+1)
 	{
+		Afisare();
+		exit(1);
+	}
+	
 		for (k = 0; k < directii; k++)
 		{
 			i_vecin = i + v_i[k];
 			j_vecin = j + v_j[k];
-			if (Verificare_poz(i_vecin, j_vecin) != 0)
+
+			if (Verificare_poz(i_vecin, j_vecin)!=0)
 			{
 				a[i_vecin][j_vecin] = pas;
 				pas++;
-				cout << pas;
-				Afisare();
-				Mutare_cal(i_vecin, j_vecin, pas);
+				Mutare_cal(i_vecin, j_vecin,pas);
 				a[i_vecin][j_vecin] = 0;
 				pas--;
 			}
 		}
-	}
 }
 
 int main()
 {
 	Citire();
-	Afisare();
-	Mutare_cal(i_init, j_init, 2);
+	Mutare_cal(i_init, j_init,2);
 	return 0;
 }
